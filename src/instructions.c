e443e7fb57480566e89ca131064c775550cd4793
Slimemaster <milo@munksgaard.eu>
2026-05-18 15:44:01 +0200

    implement branching

--- src/instructions.c
+++ src/instructions.c
@@ -1,6 +1,9 @@
 #include <stdbool.h>
 #include "cpu.h"
 
+
+char parseArgs(enum AddressingMode addressingMode, struct CPU *cpu, const unsigned char *ram, const char *rom);
+
 void compare(const unsigned char input1, const unsigned char input2, struct CPU *cpu) {
     if (input1 >= input2) {
 	cpu->statusCarry = true;
@@ -29,4 +32,20 @@ void stackPush(const unsigned char input, struct CPU *cpu, unsigned char *ram) {
 unsigned char stackPop(struct CPU *cpu, unsigned char *ram) {
     cpu->stackPointer++;
     return ram[0x100 + (short)cpu->stackPointer];
+}
+
+void branch(struct CPU *cpu, const unsigned char *ram, const char *rom) {
+    cpu->programCounter++;
+
+    short delta = parseArgs(IMMEDIATE, cpu, ram, rom);
+    delta = (delta & 0x7f) | ((delta & 0x80) << 8);
+
+    short pcOld = cpu->programCounter;
+
+    cpu->programCounter = cpu->programCounter + delta;
+
+    if ((pcOld & 0xff00) != (cpu->programCounter & 0xff00)) {
+	cpu->programCounter++;
+    }
+    
 }
--- src/instructions.h
+++ src/instructions.h
@@ -1,3 +1,4 @@
 void compare(const unsigned char input1, const unsigned char input2, struct CPU *cpu);
 void stackPush(const unsigned char input, struct CPU *cpu, unsigned char *ram);
 unsigned char stackPop(struct CPU *cpu, unsigned char *ram);
+void branch(struct CPU *cpu, const unsigned char *ram, const char *rom);
